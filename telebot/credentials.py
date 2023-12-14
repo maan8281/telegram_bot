b2de4d7950a3ec34647a6eaab3b985eb424293fa
Manmohan ishwar <immanmohan82@gmail.com>
2023-12-14 11:16:42 +0530

    changedh eee app.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "6813304809:AAHan8JX_Z53pP4Kc81ZTBcYU4iFhtWaNvI"
-bot_user_name = "everytenmin_bot"
-URL = "https://telebot-b5q4.onrender.com"
+bot_user_name = "@everytenmin_bot"
+URL = "https://telebot-b5q4.onrender.com/"
